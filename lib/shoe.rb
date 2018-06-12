7cb87911a36b5e2b92ab00c7bb729daef1910c3c
Marie Cain <mariecain27@gmail.com>
2018-06-12 03:14:43 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand)
+    if BRANDS.include?(brand)
+      true
+    else
+      BRANDS << brand  
+    end
   end
 
   def cobble
